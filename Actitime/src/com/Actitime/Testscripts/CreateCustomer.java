141f7f3d931c532e2dc810275f8769eb38947b3f
Jspiders thane1 <Jspiders thane1@Gateway-of-India>
2023-03-31 12:00:42 +0530

    Gaurav second commit

--- Actitime/src/com/Actitime/Testscripts/CreateCustomer.java
+++ Actitime/src/com/Actitime/Testscripts/CreateCustomer.java
@@ -1,5 +1,32 @@
 package com.Actitime.Testscripts;
 
-public class CreateCustomer {
+import java.io.IOException;
 
+import org.apache.poi.EncryptedDocumentException;
+import org.testng.annotations.Test;
+
+import com.Actitime.GenericLibrary.BaseClass;
+import com.Actitime.GenericLibrary.FileLibrary;
+import com.Actitime.pom.HomePage;
+import com.Actitime.pom.TaskPage;
+
+public class CreateCustomer extends BaseClass {
+	@Test
+	public void createCustomer() throws EncryptedDocumentException, IOException {
+		  HomePage hp=new HomePage(driver);  
+		  hp.getTasklnk().click();
+		  
+		  TaskPage tp=new TaskPage(driver);
+		  tp.getAddnewbtn().click();
+		  tp.getNewcust().click();
+		  FileLibrary f=new FileLibrary();
+		  String name = f.readDataFromExcelFile("Sheet1", 4, 1);
+		  tp.getCustname().sendKeys(name);
+		 String description = f.readDataFromExcelFile("Sheet1", 4, 2);
+		  tp.getCustdesp().sendKeys(description);
+		  tp.getCreatecust().click();
+		  
+		  
+		  
+	}
 }
